4278fbee9ca08d842f86c9c16eea8791e11f3345
EmilioNacato <ejnacato@espe.edu.ec>
2025-02-27 20:30:57 -0500

    Levantar localhost servicio

--- src/main/java/com/banquito/paymentprocessor/procesarcores/banquito/config/WebConfig.java
+++ src/main/java/com/banquito/paymentprocessor/procesarcores/banquito/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.banquito.paymentprocessor.procesarcores.banquito.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +15,10 @@ public class WebConfig implements WebMvcConfigurer {
             .allowedOrigins("*")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*");
+    }
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
     }
 } 
